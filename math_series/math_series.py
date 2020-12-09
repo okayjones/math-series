a923d59fa359dc5f40413154ff365cc01dd91133
Logan Jones <loganemilyjones@mac.com>
2020-12-09 10:23:09 -0800

    Fibonacci and lucas tests passing

---
+++ math_series/math_series.py
@@ -0,0 +1,20 @@
+# Fibonacci
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+        
+#Lucas
+def lucas(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n - 1) + lucas(n - 2)
+
+#Sum Series
+def sum_series()
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,5 +1,48 @@
 from math_series import __version__
-
+from math_series.math_series import fibonacci, lucas, sum_series
 
 def test_version():
     assert __version__ == '0.1.0'
+
+# Fibonacci tests
+def test_fib_zero():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
+
+def test_fib_one():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
+
+def test_fib_two():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+def test_fib_five():
+    actual = fibonacci(5)
+    expected = 5
+    assert actual == expected
+
+
+# Lucas tests
+def test_lucas_zero():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+
+def test_lucas_one():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+def test_lucas_two():
+    actual = lucas(2)
+    expected = 3
+    assert actual == expected
+
+def test_lucas_five():
+    actual = lucas(5)
+    expected = 11
+    assert actual == expected
