43a55b4b111416f0621e6b8e56bf4641537f1ebe
Logan Jones <loganemilyjones@mac.com>
2020-12-09 11:35:50 -0800

    sum_series passing

--- math_series/math_series.py
+++ math_series/math_series.py
@@ -17,4 +17,10 @@ def lucas(n):
         return lucas(n - 1) + lucas(n - 2)
 
 #Sum Series
-def sum_series()
+def sum_series(n, n1=0, n2=1):
+    if n == 0:
+        return n1
+    if n == 1:
+        return n2
+    else:
+        return sum_series(n - 1, n1, n2) + sum_series(n - 2, n1, n2)
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -46,3 +46,85 @@ def test_lucas_five():
     actual = lucas(5)
     expected = 11
     assert actual == expected
+
+
+#Sum Series
+
+## sum_series fib implicit tests
+def test_sum_series_zero():
+    actual = sum_series(0)
+    expected = 0
+    assert actual == expected
+
+def test_sum_series_one():
+    actual = sum_series(1)
+    expected = 1
+    assert actual == expected
+
+def test_sum_series_two():
+    actual = sum_series(2)
+    expected = 1
+    assert actual == expected
+
+def test_sum_series_five():
+    actual = sum_series(5)
+    expected = 5
+    assert actual == expected
+
+## sum_series fib explicit tests
+def test_sum_series_zero_zero_one():
+    actual = sum_series(0, 0, 1)
+    expected = 0
+    assert actual == expected
+
+def test_sum_series_one_zero_one():
+    actual = sum_series(1, 0, 1)
+    expected = 1
+    assert actual == expected
+
+def test_sum_series_two_zero_one():
+    actual = sum_series(2, 0, 1)
+    expected = 1
+    assert actual == expected
+
+def test_sum_series_five_zero_one():
+    actual = sum_series(5, 0, 1)
+    expected = 5
+    assert actual == expected
+
+# sum_series lucas explicit tests
+def test_sum_series_zero_two_one():
+    actual = sum_series(0, 2, 1)
+    expected = 2
+    assert actual == expected
+
+def test_sum_series_one_two_one():
+    actual = sum_series(1, 2, 1)
+    expected = 1
+    assert actual == expected
+
+def test_sum_series_two_two_one():
+    actual = sum_series(2, 2, 1)
+    expected = 3
+    assert actual == expected
+
+def test_sum_series_five_two_one():
+    actual = sum_series(5, 2, 1)
+    expected = 11
+    assert actual == expected
+
+# Other starter nums
+def test_sum_series_zero_three():
+    actual = sum_series(0, 3)
+    expected = 3
+    assert actual == expected
+
+def test_sum_series_one_three_two():
+    actual = sum_series(1, 3, 2)
+    expected = 2
+    assert actual == expected
+
+def test_sum_series_three_three_two():
+    actual = sum_series(3, 3, 2)
+    expected = 7
+    assert actual == expected
